3ddf3f6bf2a98587709878a18fd21625ebb1af5b
gvargas707 <zstryder707+work@gmail.com>
2023-07-24 23:42:57 -0500

    Adjusted error page css
    CSS selectors for error page were impacting nav elements. Added class selector to specifically target error page elements only.

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -14,7 +14,7 @@ export default function ErrorPage() {
       <h2>Uh oh! It looks like we rolled a nat one and couldn't load the page you're looking for.</h2>
       <p className="error-message">{error.statusText || error.message}</p>
       <br/>
-      <h3>Let's go <Link to={`/`}>Home</Link> and roll again.</h3>
+      <h3>Let's go <Link className="error-home" to={`/`}>Home</Link> and roll again.</h3>
     </div>
   )
 }
