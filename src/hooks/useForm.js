4402391e1cfce64458b50148a2ada5266ebf5d35
gvargas707 <zstryder707+work@gmail.com>
2023-08-08 23:36:34 -0500

    Refactoring useForm

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -2,46 +2,47 @@ import React, { useCallback, useReducer } from "react";
 
 const formReducer = (state,action) => {
     switch (action.type){
-      case 'change':
-        let formIsValid = true;
-        for (const input in state.inputs){
-          if (!state.inputs[input]){
-            continue;
-          }
-          if (input === action.input){
-            formIsValid = formIsValid && action.isValid
-          } else {
-            formIsValid = formIsValid && state.inputs[input].isValid;
-          }
-        }
-        return {
-          ...state,
-          inputs: {
-            ...state.inputs,
-            [action.input] : {value: action.value, isValid: action.isValid}
-          },
-          isValid: formIsValid
-        }
-      case 'touch':
-        console.log(state)
+      case 'UPDATE':
+        return state
+        
+      case 'TOUCH':
+        return state
       default:
         return state
     }
 }
 
-const useForm = (initialInputs, initialFormValidity) => {
+/**
+ * Custom hook to manage form inputs and overall form validity.
+ * 
+ * @param {Object[]} inputs - An array containing inputs as objects.
+ * @param {string} inputs[].input - The name or ID of the input.
+ * @param {string} inputs[].value - The value of the input
+ * @param {boolean} inputs[].isValid - The valid state of the input.
+ * @param {boolean} isFormValid - Indicates whether the entire form is valid or not. (false unless all containing inputs.isValid is true)
+ * @returns {[Object, function]} - Contains the state of the form and a function for managing that state from other components.
+ */
+
+const useForm = (
+  inputs,
+  isFormValid = false
+) => {
   const [inputStates, dispatch] = useReducer(formReducer, {
-    inputs: initialInputs,
-    isValid: initialFormValidity
+    inputs,
+    isFormValid
   })
 
   const changeHandler = useCallback((id, value, isValid) => {
-    dispatch({
-      type: 'change',
-      value,
-      isValid,
-      input: id
-    })
+    dispatch(
+      {
+        type: 'UPDATE',
+        payload: {
+          input: id,
+          value,
+          isValid
+        }
+      }
+    )
   }, []);
 
   return [inputStates, changeHandler]
